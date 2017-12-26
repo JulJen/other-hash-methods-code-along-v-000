aa5b17c60f02fd68a3ac79d52b150bac7b4cf493
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 20:43:45 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,36 +9,5 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  groceries.each do |item, item_details_hash|
-    # if attribute == :favorite_ice_cream_flavors
-    # item.each do |key, data|
-    item_details_hash.each do |attribute, data|
-      if attribute == {}
-        data.each do |key|
-        puts "#{key}"
-      end
-    end
-
-  # dairy: ["milk", "yogurt", "cheese"],
-  # vegetable: ["carrots", "broccoli", "cucumbers"],
-  # meat: ["chicken", "steak", "salmon"],
-  # grains: ["rice", "pasta"]
-  #
+groceries.key
 end
-
-
-# describe "#get_the_keys" do
-#   let :groceries do
-#     {
-#       dairy: ["milk", "yogurt", "cheese"],
-#       vegetable: ["carrots", "broccoli", "cucumbers"],
-#       meat: ["chicken", "steak", "salmon"],
-#       grains: ["rice", "pasta"]
-#     }
-#   end
-#
-#   it "returns all the keys from the hash" do
-#     result = get_the_keys(groceries)
-#     expect(result).to eq([:dairy, :vegetable, :meat, :grains])
-#   end
-# end
