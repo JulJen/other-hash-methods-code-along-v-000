d07d8064561e6239fa1c41943f401c4058ec4165
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 20:33:55 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -10,7 +10,7 @@ require 'pry'
 
 def get_the_keys(groceries)
   groceries.each do |item, item_details_hash|
-    # item_details_hash.each do |keys, data|
+    item_details_hash.each do |keys, data|
   #     if attribute == :favorite_ice_cream_flavors
   #       data.delete_if {|ice_cream| ice_cream == "strawberry"}
   #     end
