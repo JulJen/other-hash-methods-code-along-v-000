f25db0bc79aec52cf1b9bef82d264da952e92fda
Julia J-Prog <juliajen.prog@gmail.com>
2017-12-26 20:26:02 +0000

    automatically backed up by learn

--- lib/keys.rb
+++ lib/keys.rb
@@ -9,14 +9,18 @@ require 'pry'
 # }
 
 def get_the_keys(groceries)
-  # groceries.each do |item, item_details_hash|
-  #   contact_details_hash.each do |attribute, data|
+  groceries.each do |item, item_details_hash|
+    contact_details_hash.each do |attribute, data|
   #     if attribute == :favorite_ice_cream_flavors
   #       data.delete_if {|ice_cream| ice_cream == "strawberry"}
   #     end
-  #   end
-  # end
-
+    end
+  end
+  # dairy: ["milk", "yogurt", "cheese"],
+  # vegetable: ["carrots", "broccoli", "cucumbers"],
+  # meat: ["chicken", "steak", "salmon"],
+  # grains: ["rice", "pasta"]
+  #
 end
 
 
